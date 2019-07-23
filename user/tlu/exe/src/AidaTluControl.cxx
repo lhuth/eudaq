537c2a274b85257be847ff7cfc90645c98daf955
Lennart Huth <lennart.huth@desy.de>
2019-07-23 16:01:02 +0200

    renamed controller

---
+++ user/tlu/exe/src/AidaTluControl.cxx
@@ -0,0 +1,13 @@
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <cstdio>
+#include <csignal>
+#include <memory>
+#include <iostream>
+int main(int /*argc*/, char **argv) {
+
+std::cout << "Hello world :)" << std::endl;
+return 1;
+
+}
