1db2963c972c7fc476535e1e8d95afd5f0965e02
Lennart Huth <lennart.huth@desy.de>
2020-07-02 08:49:48 +0200

    fixed da fucking converter

--- user/MuPix8/module/src/MuPix8RawEvent2StdEventConverter.cc
+++ user/MuPix8/module/src/MuPix8RawEvent2StdEventConverter.cc
@@ -6,7 +6,7 @@
 class MuPix8RawEvent2StdEventConverter: public eudaq::StdEventConverter{
 public:
   bool Converting(eudaq::EventSPC d1, eudaq::StdEventSP d2, eudaq::ConfigSPC conf) const override;
-  static const uint32_t m_id_factory = eudaq::cstr2hash("ATLASPix");
+  static const uint32_t m_id_factory = eudaq::cstr2hash("MuPix8");
 };
 
 namespace{
@@ -17,11 +17,22 @@ namespace{
 bool MuPix8RawEvent2StdEventConverter::Converting(eudaq::EventSPC d1, eudaq::StdEventSP d2, eudaq::ConfigSPC conf) const{
   auto ev = std::dynamic_pointer_cast<const eudaq::RawEvent>(d1);
   auto block_n_list = ev->GetBlockNumList();
-  auto dutType = conf->Get("sensor_id",66);
-  auto dutname = conf->Get("detector","atlaspix");
+  auto dutType = conf->Get("sensor_id",3);
+  auto dutname = conf->Get("detector","Mupix8");
 
   eudaq::StandardPlane plane(0, dutname, dutname);
   d2->SetDetectorType("atlaspix");
+  int hits =0;
+  for(auto &block_n: block_n_list){
+    auto block =  ev->GetBlock(block_n);
+    TelescopeFrame tf;
+    if(!tf.from_uint8_t(ev->GetBlock(block_n)))
+        EUDAQ_ERROR("Cannot read TelescopeFrame");
+    hits+=tf.num_hits();
+  }
+  plane.SetSizeZS(128,400,hits);
+  //std::cout << hits <<std::endl;
+  hits = 0;
   for(auto &block_n: block_n_list){
     auto block =  ev->GetBlock(block_n);
     TelescopeFrame tf;
@@ -32,8 +43,12 @@ bool MuPix8RawEvent2StdEventConverter::Converting(eudaq::EventSPC d1, eudaq::Std
     for(uint i =0; i < tf.num_hits();++i)
     {
         RawHit h = tf.get_hit(i,dutType);
-        if(h.row()<381)
-            plane.PushPixel(h.column(),h.row(),1,d1->GetTimestampBegin()*1000);
+    //std::cout << h <<std::endl;
+    //std::cout << d1->GetTimestampBegin() <<std::endl;
+    //plane.PushPixel(h.column(),h.row(),1,d1->GetTimestampBegin()*1000);
+    plane.SetPixel(hits,h.column(),h.row(),1,d1->GetTimestampBegin());
+    hits++;
+    //std::cout << "Finished hit copy" <<std::endl;
     }
   }
   d2->AddPlane(plane);
